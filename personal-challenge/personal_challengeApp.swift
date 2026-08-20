7f0fd54d354707f865b7c0ac88ec5b9f35b0b0d0
Raka Febrian Syahputra <rakafebrian19@gmail.com>
2026-08-20 08:28:42 +0700

    Initial Commit

---
+++ personal-challenge/personal_challengeApp.swift
@@ -0,0 +1,32 @@
+//
+//  personal_challengeApp.swift
+//  personal-challenge
+//
+//  Created by Raka Febrian Syahputra on 20/08/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct personal_challengeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ personal-challengeTests/personal_challengeTests.swift
@@ -0,0 +1,19 @@
+//
+//  personal_challengeTests.swift
+//  personal-challengeTests
+//
+//  Created by Raka Febrian Syahputra on 20/08/26.
+//
+
+import Testing
+@testable import personal_challenge
+
+struct personal_challengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
